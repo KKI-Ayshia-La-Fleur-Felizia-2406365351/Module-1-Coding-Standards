a1e67d8b10077a095b1071805b2ab70f36e5814b
ayshialafleur <ayshialafleur@gmail.com>
2026-03-10 13:06:41 +0700

    [RED] Add OrderRepository skeleton

--- src/test/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepositoryTest.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class OrderRepositoryTest {
     OrderRepository orderRepository;
 
-    List<Order> order;
+    List<Order> orders;
 
     @BeforeEach
     void setUp() {
